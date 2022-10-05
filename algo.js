9a6c0e27c158a3eddb68b86f6ddc46861b26bf29
Samsou1 <sam.moreau0@gmail.com>
2022-10-05 10:39:39 +0200

    pascalCase in algo

--- algo.js
+++ algo.js
@@ -6,32 +6,32 @@ floor1 = [3, 7, 8, 3, 6, 1];
 floor2 = [1, 4, 5, 8];
 floor3 = [10, 9, 7, 8, 4, 5, 3, 1];
 
-console.log(sum_quad([...num1], 17));
-console.log(sum_quad([...num2], 19));
-console.log(sum_quad([...num3], 32));
+console.log(sumQuad([...num1], 17));
+console.log(sumQuad([...num2], 19));
+console.log(sumQuad([...num3], 32));
 
-console.log(floor_quad([...floor1]));
-console.log(floor_quad([...floor2]));
-console.log(floor_quad([...floor3]));
+console.log(floorQuad([...floor1]));
+console.log(floorQuad([...floor2]));
+console.log(floorQuad([...floor3]));
 
-console.log(sum_linear([...num1], 17));
-console.log(sum_linear([...num2], 19));
-console.log(sum_linear([...num3], 32));
+console.log(sumLinear([...num1], 17));
+console.log(sumLinear([...num2], 19));
+console.log(sumLinear([...num3], 32));
 
-console.log(floor_linear([...floor1]));
-console.log(floor_linear([...floor2]));
-console.log(floor_linear([...floor3]));
+console.log(floorLinear([...floor1]));
+console.log(floorLinear([...floor2]));
+console.log(floorLinear([...floor3]));
 
-console.log(sum_const([...num1], 17));
-console.log(sum_const([...num2], 19));
-console.log(sum_const([...num3], 32));
+console.log(sumConst([...num1], 17));
+console.log(sumConst([...num2], 19));
+console.log(sumConst([...num3], 32));
 
-console.log(floor_const([...floor1]));
-console.log(floor_const([...floor2]));
-console.log(floor_const([...floor3]));
+console.log(floorConst([...floor1]));
+console.log(floorConst([...floor2]));
+console.log(floorConst([...floor3]));
 
 
-function sum_quad(ary, int){
+function sumQuad(ary, int){
   for(var i = 0; i < ary.length - 1; i++){
     for(var j = i + 1; j < ary.length; j++){
       if(ary[i] + ary[j] == int){
@@ -42,7 +42,7 @@ function sum_quad(ary, int){
   return false
 }
 
-function floor_quad(ary){
+function floorQuad(ary){
   var num = 0;
   for(var i = 0; i < ary.length; i++){
     var taller = true;
@@ -58,7 +58,7 @@ function floor_quad(ary){
   return num;
 }
 
-function sum_linear(ary, int){
+function sumLinear(ary, int){
   var hash = {};
   for(var i = 0; i < ary.length; i++){
     if(!hash[ary[i]]){
@@ -76,7 +76,7 @@ function sum_linear(ary, int){
   return false;
 }
 
-function floor_linear(ary){
+function floorLinear(ary){
   var num = 1;
   ary = ary.reverse();
   var max = ary[0];
@@ -89,7 +89,7 @@ function floor_linear(ary){
   return num;
 }
 
-function sum_const(ary,int){
+function sumConst(ary,int){
   hash = {};
   for(var i = 0; i < ary.length; i++){
     if(hash[int - ary[i]] != undefined){
@@ -102,7 +102,7 @@ function sum_const(ary,int){
 }
 
 
-function floor_const(ary){
+function floorConst(ary){
   var num = 1;
   var max = ary[ary.length - 1];
   for(var i = ary.length - 1; i >= 0 ; i--){
