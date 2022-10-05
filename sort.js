9d598ad55b34bdd6720e861f40a4f8aff5e250c2
Samsou1 <sam.moreau0@gmail.com>
2022-10-05 10:00:18 +0200

    merge both sorting files

--- sort.js
+++ sort.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const fileName = process.argv[2];
 
-var list;
+let list;
 try {
     const data = fs.readFileSync(fileName, 'utf8');
     list = data.split(' ').map(element => parseFloat(element));
@@ -9,10 +9,87 @@ try {
     console.error(error.message);
 }
 
-var merge_comparison = 0;
+console.log(bubble_sort([...list]));
+console.log(insertion_sort([...list]));
+console.log(selection_sort([...list]));
+let comparison_quick_sort;
+console.log(quick_sort([...list]));
+let merge_comparison;
 console.log(merge_sort([...list]));
 
+function bubble_sort(ary){
+    var comparison = 0;
+    for(var i = 0; i < ary.length; i++){
+        for(var j = 0 ; j < ary.length - i - 1 ; j++){
+            if(ary[j]>ary[j+1]){
+                [ary[j], ary[j+1]] = [ary[j+1], ary[j]];
+            }
+            comparison++;
+        }
+    }
+    return `Bubble sort: ${comparison} comparisons ` + ary;
+}
+
+function insertion_sort(ary){
+    var comparison = 0;
+    for(var i = 1; i < ary.length; i++){
+        let current_value = ary[i];
+        let j = i - 1;
+        while ((j >= 0) && (current_value < ary[j])) {
+            ary[j+1] = ary[j];
+            j--;
+            comparison++;
+        }
+        ary[j+1] = current_value;
+    }
+    return `Insertion sort: ${comparison} comparisons ` + ary;
+}
+
+function selection_sort(ary){
+    var comparison = 0;
+    for(var i = 0; i < ary.length; i++){
+        var min = ary[i];
+        for(var j = i + 1; j < ary.length; j++){
+            if(ary[j] < min){
+                min = ary[j];
+            }
+            comparison++;
+        }
+        ary.splice(i, 0, min);
+        // Here we look for the value min to destroy it but we have to be careful not to delete the first value we just copied.
+        // Hence the i + 1 stating that we are only looking for the value after index i + 1
+        ary.splice(ary.indexOf(min, i + 1), 1)
+    }
+    return `Selection sort: ${comparison} comparisons ` + ary;
+}
+
+function quick_sort(ary){
+    comparison_quick_sort = 0;
+    var result = qs(ary);
+    return `Quick sort: ${comparison_quick_sort} comparisons ` + result;
+}
+
+function qs(ary){
+    if(ary.length <= 1){
+        return ary;
+    }
+    var pivot = ary.pop();
+    var greater_than_pivot = [];
+    var smaller_than_pivot = [];
+    ary.map(function(element){
+        if(element < pivot){
+            smaller_than_pivot.push(element);
+        }else{
+            greater_than_pivot.push(element);
+        }
+        comparison_quick_sort++;
+    })
+    return (qs(smaller_than_pivot).concat([pivot], qs(greater_than_pivot)));
+}
+
+
 function merge_sort(ary){
+  merge_comparison = 0;
   ary = ms(ary);
   return `Merge sort: ${merge_comparison} comparisons ` + ary;
 }
